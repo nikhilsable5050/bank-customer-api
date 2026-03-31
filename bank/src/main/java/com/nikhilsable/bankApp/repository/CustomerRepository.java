17bf041d9daebc578e5ae6eb9dc19ceab05d7254
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-03-31 12:42:50 +0530

    add Repo annotation

--- bank/src/main/java/com/nikhilsable/bankApp/repository/CustomerRepository.java
+++ bank/src/main/java/com/nikhilsable/bankApp/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.nikhilsable.bankApp.repository;
 
 import com.nikhilsable.bankApp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
