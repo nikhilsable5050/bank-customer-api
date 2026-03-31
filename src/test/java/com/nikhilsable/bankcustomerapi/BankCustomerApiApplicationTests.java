f840200dc5ef9d1d618bb5ce1c2423a7fb4c8bb2
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-03-31 12:14:10 +0530

    add backend project

---
+++ src/test/java/com/nikhilsable/bankcustomerapi/BankCustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.bankcustomerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
